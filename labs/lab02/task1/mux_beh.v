0228ddf67cc8471eeb79828e3ba7a5b6d384037e
sidh1018 <f20241018@pilani.bits-pilani.ac.in>
2026-09-18 16:22:20 +0000

    lab02 task1

--- labs/lab02/task1/mux_beh.v
+++ labs/lab02/task1/mux_beh.v
@@ -1,15 +1,8 @@
-// mux_beh.v
-// 2-to-1 multiplexer, BEHAVIORAL style.
-//
-// This file does not compile as-is. Find the bug and fix it before moving on.
-// Hint: think carefully about which port should be a net and which should be
-// a variable in behavioral modeling.
-
 module mux_beh (
-  input       I0,
-  input       I1,
-  input       S,
-  output wire Y
+  input      I0,
+  input      I1,
+  input      S,
+  output reg Y
 );
 
   always @(*) begin
