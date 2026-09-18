3bf35643cfef4f5b22fae1eab867e4ed3d144cc9
sidh1018 <f20241018@pilani.bits-pilani.ac.in>
2026-09-18 16:18:50 +0000

    lab02 task1

--- labs/lab02/task1/mux_df.v
+++ labs/lab02/task1/mux_df.v
@@ -1,8 +1,15 @@
+// mux_df.v
+// 2-to-1 multiplexer, DATAFLOW style.
+//
+// This file does not compile as-is. Find the bug and fix it before moving on.
+// Hint: think carefully about which port should be a net and which should be
+// a variable in dataflow modeling.
+
 module mux_df (
-  input       I0,
-  input       I1,
-  input       S,
-  output wire Y
+  input      I0,
+  input      I1,
+  input      S,
+  output reg Y
 );
 
   assign Y = S ? I1 : I0;
